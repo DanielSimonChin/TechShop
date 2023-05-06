f9edf761ea2d771f975add9debc2eaf63641ffb9
Daniel Chin <danielsimonchin@hotmail.com>
2023-05-06 02:13:54 -0400

    started Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Container } from "react-bootstrap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <h1>Welcome</h1>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    )
 }
 
-export default App;
+export default App
