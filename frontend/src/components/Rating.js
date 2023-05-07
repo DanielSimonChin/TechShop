09f26b5b88050edd2a2d1e116115d13e05c103f4
Daniel Chin <danielsimonchin@hotmail.com>
2023-05-07 12:56:25 -0400

    Added nodemon, concurrently, env variables and ES modules

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,4 @@
 import React from "react"
-import PropTypes from "prop-types"
 
 const Rating = ({ value, text, color }) => {
     return (
@@ -72,12 +71,6 @@ const Rating = ({ value, text, color }) => {
 
 Rating.defaultProps = {
     color: "#f8e825"
-}
-
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string
 }
 
 export default Rating
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ const ProductScreen = ({ props }) => {
         }
 
         fetchProduct()
-    }, [])
+    }, [id])
 
     return (
         <>
