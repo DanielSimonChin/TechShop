9d664e009d3d59850441d9f6d9708b33f95bf483
Daniel Chin <danielsimonchin@hotmail.com>
2024-01-04 20:44:04 -0500

    removed loader from carousel component

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
-import Loader from "./Loader";
 import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant="danger">{error}</Message>
+  return isLoading ? null : error ? (
+    <Message variant="danger">{error?.data?.message || error.error}</Message>
   ) : (
     <Carousel pause="hover" className="bg-primary mb-4">
       {products.map((product) => (
@@ -18,7 +15,7 @@ const ProductCarousel = () => {
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className="carousel-caption">
-              <h2>
+              <h2 className="text-white text-right">
                 {product.name} (${product.price})
               </h2>
             </Carousel.Caption>
