015083ddcf12557175df6c2389e917df3b13026c
Daniel Chin <danielsimonchin@hotmail.com>
2024-01-07 02:17:44 -0500

    fixxxx

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 });
