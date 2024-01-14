5239b8bcc9a01ff9366cbc74c64c4ddd7b554cd7
Daniel Chin <danielsimonchin@hotmail.com>
2024-01-13 23:15:59 -0500

    added domain property to cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -12,6 +12,7 @@ const generateToken = (res, userId) => {
     sameSite: "None",
     maxAge: 60 * 60 * 24 * 30, // 30 days
     path: "/",
+    domain: "vercel.app",
   });
 
   res.setHeader("Set-Cookie", serializedToken);
